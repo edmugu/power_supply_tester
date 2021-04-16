4c6a89d06b8c21027a14f438bd574dc4e6be721a
Eduardo Munoz-Gutierrez <edmugu@users.noreply.github.com>
2021-04-15 19:47:52 -0700

    Create board.py

---
+++ scripts/board.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Controls the tester board.
+
+EXAMPLE: 
+        python board.py --port=COM3 
+
+
+REQUIREMENTS:
+    python 3.7+
+    pyfirmata  (to talk to the arduino)
+    fire (to create command line tool)
+
+@author: Eduardo Munoz
+@email: edmugu@protonmail.com
+"""
+import pyfirmata
+import fire
+import time
+
+
+class Board(object):
+    """
+    It controls the power supply tester board
+    """
+
+	
