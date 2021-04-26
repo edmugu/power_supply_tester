9d4e0f92c3987f2f953511d74b72518f90cf7c86
Eduardo Munoz-Gutierrez <edmugu@users.noreply.github.com>
2021-04-25 17:58:17 -0700

    Adds methods
    The read_vload and set_vload is added

--- scripts/board.py
+++ scripts/board.py
@@ -5,21 +5,19 @@ Controls the power supply tester board.
 The main component of the tester is the power transistor that will control
 the current/power drawn from the power supply to be tested. This current
 is controlled by the voltage at the gate of the transistor. And that voltage 
-is set by two voltage set by the arduino "coarse voltage" and "fine voltage."
+is set by the arduino pwm output
 
-
-           
-                                             ▲  Power supply to Test
+                                             ▲ Power supply to Test
                                              │    Max voltage 40V
                                      ┌───────┤
    Vset   ┌────────────────┐         │       │
     ▲     │            Vcc ├─────────┘   │ ┌─┘
     │     │  x10 Gain Amp  │             │ │    Power Transistor Gate
     └─────┤Input     Output├─────────────┤ │      
-          │  __            │  0 to 40 V  │ └─┐
-          └────────────────┘  Vgate      │   │
-                                             │
-                                             │
+          │                │  0 to 40 V  │ └─┐
+          └────────────────┘  Vgate      │   │   ▲  Vload
+                                             │   │
+                                             │───┘
                                             ┌┴┐
                                             │ │ Power Resistor
                                             │ │ Resistance ~10 ohm
@@ -27,26 +25,20 @@ is set by two voltage set by the arduino "coarse voltage" and "fine voltage."
                                              │
                                              ▼
 
-
-
-Arduino
-────────┐        ┌────────────┐      ┌────────────┐        Vset
-        │        │            │      │   Op Amp   │          ▲
-Coarse  ├────────┤   Low-Pass ├──────┤   Voltage  │          │
-Voltage │        │    Filter  │      │    Adder   ├──────────┘
-        │        │            │   ┌──┤            │      
-        │        └────────────┘   │  └────────────┘      
-        │                         │                      
-        │                         └───────────────────┐  
-        │                                             │  
-        │        ┌────────────┐      ┌────────────┐   │  
-        │        │            │      │   x0.1     │   │  
-Fine    ├────────┤   Low-Pass ├──────┤  Voltage   ├───┘  
-Voltage │        │    Filter  │      │  Divider   │      
-        │        │            │      │            │      
-────────┘        └────────────┘      └────────────┘      
-
-
+Arduino                               Vset
+────────┐        ┌────────────┐         ▲
+        │        │            │         │
+PWM     ├>──────>┤   Low-Pass ├>────────┘
+Voltage │        │    Filter  │
+        │        │            │
+        │        └────────────┘
+        │
+        │
+        │        ┌────────────┐   ▲  Vload
+Vload   │        │   x0.1     │   │
+sense   │<──────<┤  Voltage   ├<──┘
+        │        │  Divider   │
+────────┘        └────────────┘
 
 EXAMPLE: 
         python board.py --port=COM3 
@@ -70,17 +62,67 @@ class Board(object):
     It controls the power-supply-tester board.
     """
 
-    def __init__(self, pin_vcoarse, pin_vfine, amp_gain, resistance, vthresh):
+    def __init__(
+        self,
+        port="COM4",
+        pin_vset=3,
+        pin_vload=0,
+        amp_gain=10,
+        resistance=10,
+        vthresh=0.5,
+    ):
         self.board = pymata4.Pymata4()
 
-        self.pin_vcoarse = self.board.set_pin_mode_pwm_output(pin_vcoarse)
-        self.pin_vfine = self.board.set_pin_mode_pwm_output(pin_vfine)
+        self.pin_vset = pin_vset
+        self.pin_vload = pin_vload
         self.amp_gain = amp_gain
         self.resistance = resistance
         self.vthresh = vthresh
+
+        self.board.enable_analog_reporting(pin_vload)
+        self.board.set_pin_mode_pwm_output(pin_vset)
+        self.pin_voltage_value = 0  # this value is from 0 to 0x4000
+        self.value_to_voltage = 5.2 / 1024
+
+    def read_vload(self):
+        """
+        It reads the voltage on the load
+        """
+        time.sleep(0.1)
+        value = self.board.analog_read(self.pin_vload)
+        value = self.board.analog_read(self.pin_vload)
+        value = self.value_to_voltage * value[0]  # throw timestap away
+        return value
+
+    def set_vload(self, voltage, verbose=True):
+        """
+        It sets the voltage on the power resistor. It does that by raising the
+        voltage on the gate of the transistor until it reaches the target
+        voltage.
+
+        :param voltage: voltage to set
+        """
+        if verbose:
+            print("Setting voltage to %5.3f volts." % voltage)
+
+        for n in range(0x4000):
+            self.board.pwm_write(self.pin_vset, n)
+            self.pin_voltage_value = n
+            value = self.read_vload()
+            if verbose:
+                print("Voltage read: %5.3f" % value)
+            if value > voltage:
+                break
+            if verbose:
+                for _ in range(3):
+                    print("Voltage read: %5.3f" % value)
 
     def print(self):
         """
         print board
         """
         print("test")
+
+
+if __name__ == "__main__":
+    fire.Fire(Board)
